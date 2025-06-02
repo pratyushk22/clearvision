e93dc3d8e2936d794780c1b571897480b0e79ff3
Pratyush Kumar Sethy <159880196+pratyushk22@users.noreply.github.com>
2025-06-02 14:24:53 +0530

    Delete models/generator.py

--- models/generator.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-
-class DummyGenerator(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.model = nn.Identity() 
-
-    def forward(self, x):
-        return x
